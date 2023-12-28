55e338091a6a10257d632fcfcfa153bb76f38fac
Tim Standen <timmstanden@gmail.com>
2023-12-28 12:13:35 -0800

    Ch 6.3 improve router API

--- index.ts
+++ index.ts
@@ -2,6 +2,18 @@ export * from "./lib/logtar.ts"
 
 const PORT = 5255;
 
+enum HTTP_METHODS {
+  GET = "GET",
+  POST = "POST",
+  PUT = "PUT",
+  DELETE = "DELETE",
+  PATCH = "PATCH",
+  HEAD = "HEAD",
+  OPTIONS = "OPTIONS",
+  CONNECT = "CONNECT",
+  TRACE = "TRACE"
+}
+
 class Router {
   #routes: {
     [key: string]: (request: Request) => Response | void
@@ -21,8 +33,36 @@ class Router {
     return handler(request);
   }
 
-  addRoute(method: string, path: string, handler: () => Response | void) {
+  #addRoute(method: string, path: string, handler: (request?: Request) => Response | void) {
     this.#routes[`${method} ${path}`] = handler;
+  }
+
+  get(path: string, handler: (request?: Request) => Response) {
+    this.#addRoute(HTTP_METHODS.GET, path, handler);
+  }
+  post(path: string, handler: (request?: Request) => Response) {
+    this.#addRoute(HTTP_METHODS.POST, path, handler);
+  }
+  put(path: string, handler: (request?: Request) => Response){
+    this.#addRoute(HTTP_METHODS.PUT, path, handler);
+  }
+  delete(path: string, handler: (request?: Request) => Response){
+    this.#addRoute(HTTP_METHODS.DELETE, path, handler);
+  }
+  patch(path: string, handler: (request?: Request) => Response){
+    this.#addRoute(HTTP_METHODS.PATCH, path, handler);
+  }
+  head(path: string, handler: (request?: Request) => Response){
+    this.#addRoute(HTTP_METHODS.HEAD, path, handler);
+  }
+  options(path: string, handler: (request?: Request) => Response){
+    this.#addRoute(HTTP_METHODS.OPTIONS, path, handler);
+  }
+  connect(path: string, handler: (request?: Request) => Response){
+    this.#addRoute(HTTP_METHODS.CONNECT, path, handler);
+  }
+  trace(path: string, handler: (request?: Request) => Response){
+    this.#addRoute(HTTP_METHODS.TRACE, path, handler);
   }
 
   printRoutes(){
@@ -36,9 +76,9 @@ function handlePostBasePath(){
   return new Response("Hello POST", {status: 201});
 }
 const router = new Router();
-router.addRoute('GET', '/', handleGetBasePath);
+router.get('GET', handleGetBasePath);
 
-router.addRoute('POST', '/', handlePostBasePath); 
+router.post('POST', handlePostBasePath); 
 
 Bun.serve(
   {
