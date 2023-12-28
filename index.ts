0d5f0076d42f13a96946107714e8b5eef7dca26b
Tim Standen <timmstanden@gmail.com>
2023-12-28 11:43:20 -0800

    CH 6.2 basic router

--- index.ts
+++ index.ts
@@ -2,54 +2,52 @@ export * from "./lib/logtar.ts"
 
 const PORT = 5255;
 
-const initBaseHeaders = (): Headers => {
-  const headers = new Headers();
-  headers.append("Content-Type", "text/plain");
-  return headers
-};
+class Router {
+  #routes: {
+    [key: string]: (request: Request) => Response | void
+  } = {};
 
-const getRouteHandler = (route: string): () => Response => {
-  switch (route) {
-  case "GET /": 
-    return () => {
-      const status = 200;
-      const data = "Hello World!";
-      const headers = initBaseHeaders(); 
-      headers.append("My-Header", "Hello World");
-      return new Response(data, {status, headers});
-    }
-  case "POST /echo": 
-    return () => {
-      const status = 201;
-      const data = "Yellow World!";
-      const headers = initBaseHeaders(); 
-      headers.append("My-Header", "Yellow World!");
-      return new Response(data, {status, headers});
-    }
-  default:
-    return () => {
-      const status = 404;
-      const data = "Not Found";
-      const headers = initBaseHeaders();
-      return new Response(data, {status, headers});
-    }
+  constructor() {
+    this.#routes = {}
   }
-};
 
-function handleRequest(request: Request) {
-  const method = request.method;
-  const url = new URL(request.url);
-  const pathname = url.pathname
-  return getRouteHandler(`${method} ${pathname}`)(); 
+  handleRequest(request: Request) {
+    const method = request.method;
+    const url = new URL(request.url);
+    const handler = this.#routes[`${method} ${url.pathname}`];
+    if (!handler) {
+      return new Response("Not Found", {status: 404});
+    }
+    return handler(request);
+  }
+
+  addRoute(method: string, path: string, handler: () => Response | void) {
+    this.#routes[`${method} ${path}`] = handler;
+  }
+
+  printRoutes(){
+    console.log(Object.entries(this.#routes));
+  }
 }
+function handleGetBasePath(){
+  return new Response("Hello World", {status: 200});
+}
+function handlePostBasePath(){
+  return new Response("Hello POST", {status: 201});
+}
+const router = new Router();
+router.addRoute('GET', '/', handleGetBasePath);
+
+router.addRoute('POST', '/', handlePostBasePath); 
 
 Bun.serve(
   {
     port: PORT,
     development: true,
     fetch(req) {
-      return handleRequest(req);
+      return router.handleRequest(req);
     }
   }
 );
+
 console.log(`Server is running on port ${PORT}`);
